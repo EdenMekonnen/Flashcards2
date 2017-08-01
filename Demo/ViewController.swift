d3d44758c12a8053a8dddd81dd14358731abb83b
Eden Mekonnen <edenmekonnen@dna0f86af.sunet>
2017-08-01 10:46:44 -0700

    j

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var QuestionLabel: UILabel!
+    @IBAction func ButtonPressed(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
